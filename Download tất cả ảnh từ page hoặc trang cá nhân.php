979493bd8a46aba744308ad03ea54f913217b531
w3dding <65348951+w3dding@users.noreply.github.com>
2020-12-19 04:25:05 +0700

    Add files via upload

---
+++ Download tất cả ảnh từ page hoặc trang cá nhân.php
@@ -0,0 +1,74 @@
+<?php
+ini_set('max_execution_time', 0);
+//token bạn
+$token = "EAAAAZAw4FxQIBANd1PZBKemt8HLMgZB3LtFZB1KAR09rIy9T2nbI9mYRvklzN5TzzCTvrGGFEYpisY8pWoS4TA6cZCEFBAfY7XPMcnW4h9tULfqlNUzO2x7FxnHc21jRTmjXldxsYHQ4YCJPyj4aniI9iIYnK8PbhLe3MnL1lYAZDZD";
+//điền ID trang page hoặc cá nhân profile
+$id ="1048678438888760";
+
+//không sửa ở dưới
+$link    = "https://graph.facebook.com/$id/albums?fields=id&limit=100&access_token=$token"; 
+$array_album = array();
+while (true) {
+    $curl    = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $link,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false
+    ));
+    $response = curl_exec($curl);
+    curl_close($curl);
+    $data  = json_decode($response,JSON_UNESCAPED_UNICODE);
+    $datas = $data["data"];
+    if(count($datas)==0) break;
+    foreach($datas as $each){
+        array_push($array_album,$each['id']);
+    }
+    if(!empty($data["paging"]["next"])){
+        $link = $data["paging"]["next"];
+    }
+    else{
+        break;
+    }
+} 
+foreach ($array_album as $id_album) {
+  $link    = "https://graph.facebook.com/$id_album/photos?fields=images.source&limit=100&access_token=$token";
+  while (true) {
+    $curl    = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $link,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false
+    ));
+    $response = curl_exec($curl);
+    curl_close($curl);
+    $data  = json_decode($response,JSON_UNESCAPED_UNICODE);
+    $datas = $data["data"];
+    if(count($datas)==0) break;
+    foreach($datas as $each){
+        $id    = $each['id'];
+        $fp    = fopen ("download/$id.jpg" , 'w+');
+        $links = $each['images'][0]['source'];
+        $curls = curl_init();
+        curl_setopt_array($curls, array(
+            CURLOPT_URL => $links,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_FILE => $fp
+        ));
+        curl_exec($curls);
+        curl_close($curls);
+        fclose($fp);
+    }
+    if(!empty($data["paging"]["next"])){
+        $link = $data["paging"]["next"];
+    }
+    else{
+        break;
+    }
+  }
+}
